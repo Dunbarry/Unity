8622d4a52ce74a77174296b160e7baa47a855d86
Dunbarry <RyanADunbar@gmail.com>
2016-11-17 21:17:00 -0700

    :tada: I have made fire

--- TutorialScripts/TutorialCharacterController.cs
+++ TutorialScripts/TutorialCharacterController.cs
@@ -7,56 +7,56 @@ public class CharacterController : MonoBehaviour {
 	public float forwardVel = 12;
 	public float rotateVel = 100;
 
-  Quaternion targetRotation;
-  Rigidbody rBody;
-  float forwardInput, turnInput;
+	Quaternion targetRotation;
+	Rigidbody rBody;
+	float forwardInput, turnInput;
 
-  public Quaternion TargetRotation
-  {
-    get {return targetRotation;}
-  }
+	public Quaternion TargetRotation
+	{
+		get {return targetRotation;}
+	}
 
-  void Start()
-  {
-    targetRotation = transorm.rotation;
-    if (GetComponent<Rigidbody>())
-      rBody = GetComponent<Rigidbody>();
-    else
-      Debug.LogError("The character needs a rigid body.")
+	void Start()
+	{
+		targetRotation = transform.rotation;
+		if (GetComponent<Rigidbody> ())
+			rBody = GetComponent<Rigidbody> ();
+		else
+			Debug.LogError ("The character needs a rigid body.");
 
-      forwardInput = turnInput = 0;
-  }
+			forwardInput = turnInput = 0;
+	}
 
-  void GetInput()
-  {
-    forwardInput = input.GetAxis("Vertical");
-    turnInput = input.GetAxis("Horizontal");
-  }
+	void GetInput()
+	{
+		forwardInput = Input.GetAxis("Vertical");
+		turnInput = Input.GetAxis("Horizontal");
+	}
 
-  void Update()
-  {
-    GetInput();
-    Turn();
-  }
+	void Update()
+	{
+		GetInput();
+		Turn();
+	}
 
-  void FixedUpdate()
-  {
-    Run();
-  }
+	void FixedUpdate()
+	{
+		Run();
+	}
 
-  void Run()
-  {
-    if (mathf.Abs(forwardInput) > inputDelay)
-    {
-      // move
-      rBody.velocity = transform.forward * forwardInput * forwardVel;
-    }
-    else
-      // zero velocity
-      rBody.velocity = Vector3.zero;
-  }
+	void Run()
+	{
+		if (Mathf.Abs(forwardInput) > inputDelay)
+		{
+			// move
+			rBody.velocity = transform.forward * forwardInput * forwardVel;
+		}
+		else
+			// zero velocity
+			rBody.velocity = Vector3.zero;
+	}
 
-  void Turn()
+	void Turn()
 	{
 		if (Mathf.Abs (turnInput) > inputDelay)
 		{
